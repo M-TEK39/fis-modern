6dd4c4933e343b72e41483639cae4cfdc1744832
m-tek39 <murcus39@gmail.com>
2026-09-07 17:45:54 +0200

    fix: preserve private hire route aliases

---
+++ src/Services/FIS.Web.Next/app/private-hire/demo-vehicles/page.tsx
@@ -0,0 +1,5 @@
+import DemoVehicleMenuPage from "@/app/vehicles/demo/menu/page";
+
+export default function PrivateHireDemoVehiclesAlias() {
+  return <DemoVehicleMenuPage routePath="/private-hire/demo-vehicles" />;
+}
