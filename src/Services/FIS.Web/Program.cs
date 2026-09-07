4982fbaf0863ee7a42a23bb02e1c21a8e037e5e7
m-tek39 <murcus39@gmail.com>
2026-09-07 17:04:21 +0200

    refactor: remove fuel cards blazor surface

--- src/Services/FIS.Web/Program.cs
+++ src/Services/FIS.Web/Program.cs
@@ -159,12 +159,6 @@ builder.Services.AddHttpClient<PrivateHireApiService>(client =>
     client.Timeout = TimeSpan.FromSeconds(30);
 });
 
-builder.Services.AddHttpClient<PrivateHireFuelCardApiService>(client =>
-{
-    client.BaseAddress = apiBaseUri;
-    client.Timeout = TimeSpan.FromSeconds(30);
-});
-
 builder.Services.AddHttpClient<AuditApiService>(client =>
 {
     client.BaseAddress = apiBaseUri;
@@ -172,12 +166,6 @@ builder.Services.AddHttpClient<AuditApiService>(client =>
 });
 
 builder.Services.AddHttpClient<AccidentApiService>(client =>
-{
-    client.BaseAddress = apiBaseUri;
-    client.Timeout = TimeSpan.FromSeconds(30);
-});
-
-builder.Services.AddHttpClient<FleetManagementApiService>(client =>
 {
     client.BaseAddress = apiBaseUri;
     client.Timeout = TimeSpan.FromSeconds(30);
